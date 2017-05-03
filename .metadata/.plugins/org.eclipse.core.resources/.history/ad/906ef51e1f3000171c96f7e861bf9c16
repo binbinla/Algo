f943b0ffcf6003b25071af9cd78e7014d5b1a1eb
binbin <18826136910@163.com>
2017-05-04 00:48:30 +0800

    countWord 输入一个字符串，输出字符串当中出现的单词以及单词出现的次数。

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ad/906ef51e1f3000171c96f7e861bf9c16
@@ -0,0 +1,10 @@
+package com.binbin.algo;
+
+public class countwords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- countWord/src/com/binbin/algo/countwords.java
+++ countWord/src/com/binbin/algo/countwords.java
@@ -1,10 +1,32 @@
 package com.binbin.algo;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class countwords {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please input a String: ");
+		String in = input.nextLine();
+		input.close();
+		HashMap<String,Integer> map = new HashMap<String,Integer>();	
+		String[] stringArray = in.split(" ");
+		
+		String word = null;
+		for(String x : stringArray){
+			word = x.trim();
+			if(word.equals(" ")) continue;
+			if(map.containsKey(word)){
+				map.put(word, map.get(word)+1);
+			}else{
+				map.put(word, 1);
+			}
+		}
+		for(String res : map.keySet()){
+			System.out.printf("The inpput string contains word %s %s times\n",res,map.get(res));
+		}
 	}
 
 }
